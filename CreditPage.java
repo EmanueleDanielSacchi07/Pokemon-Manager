08c0b3b4af09afa497e998baed04df537d06d792
angisman <gurrajangelomaria@gmail.com>
2026-05-10 22:20:37 +0200

    credit page
    credit page finita, 6767676767 SEI SETTE

--- CreditPage.java
+++ CreditPage.java
@@ -14,10 +14,36 @@ public class CreditPage extends JPanel{
     CreditPage(MainController controller) {
         this.setBackground(BG_DARK);
         this.setLayout(new BorderLayout());
-        
+
+        JPanel pnlCrediti = new JPanel(new GridLayout(3, 1));
+        pnlCrediti.setBackground(BG_DARK);
+
+        JLabel lblTitolo = new JLabel("Crediti", SwingConstants.CENTER);
+        lblTitolo.setFont(new Font("Arial", Font.BOLD, 26));
+        lblTitolo.setForeground(ACCENT_YELLOW);
+        lblTitolo.setBackground(BG_DARK);
+        lblTitolo.setOpaque(true);
+
+        JLabel lblNomi = new JLabel("Sacchi Emanuele  &  Angelomaria Gurraj", SwingConstants.CENTER);
+        lblNomi.setFont(new Font("Arial", Font.BOLD, 16));
+        lblNomi.setForeground(TEXT_WHITE);
+        lblNomi.setBackground(BG_DARK);
+        lblNomi.setOpaque(true);
+
+        JLabel lblClasse = new JLabel("Classe 4^G  ISIS Bernocchi  -  A.S. 2025-2026", SwingConstants.CENTER);
+        lblClasse.setFont(new Font("Arial", Font.PLAIN, 12));
+        lblClasse.setForeground(new Color(130, 130, 155));
+        lblClasse.setBackground(BG_DARK);
+        lblClasse.setOpaque(true);
+
+        pnlCrediti.add(lblTitolo);
+        pnlCrediti.add(lblNomi);
+        pnlCrediti.add(lblClasse);
+
         btnIndietro = creaBottone("Indietro", ACCENT_RED);
         btnIndietro.addActionListener(new PageSwitchListener(controller, "main"));
 
+        this.add(pnlCrediti, BorderLayout.CENTER);
         this.add(btnIndietro, BorderLayout.SOUTH);
     
     }
